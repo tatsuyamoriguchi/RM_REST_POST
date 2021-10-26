7475858e0f3e9da139fda718fe223d35547fe3f1
Brian Moriguchi <brian@beckos.com>
2021-10-26 16:06:02 -0700

    Initial Commit

---
+++ RM_REST_POST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RM_REST_POST
+//
+//  Created by Tatsuya Moriguchi on 10/26/21.
+//  Copyright © 2021 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
